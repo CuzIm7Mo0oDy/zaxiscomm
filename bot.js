d8d28dbf24d498be65c1f09f005eb6ecf4c15679
HactorMC <44316275+HactorMC@users.noreply.github.com>
2018-12-06 11:40:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,7 +211,7 @@ var prefix = "-";
 });
 
 client.on('message',async msg => {
-  var p = "!";
+  var p = "-";
   if(msg.content.startsWith(p + "ocount")) {
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **No Permissions**');
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
@@ -287,54 +287,28 @@ if (command == "embed") {
 
 });
 
-
-client.on("ready", () => {
-if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support Team");
-        let role2 = message.guild.roles.find("name", "@everyone");
-        c.overwritePermissions(role, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        c.overwritePermissions(role2, {
-            SEND_MESSAGES: false,
-            READ_MESSAGES: false
-        });
-        c.overwritePermissions(message.author, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-        const embed = new Discord.RichEmbed()
-        .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
-        .setTimestamp();
-        c.send({ embed: embed });
-    }).catch(console.error);
-}
-if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
-
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === '-confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
-    });
-}
-
+client.on("message", message => {
+        var prefix = "-";//البرفكس
+    if(message.content.startsWith(prefix + "setwlc")) {
+        let args = message.mentions.channels.first();
+            if(!args) message.channel.send("** منشن روم . :x:**");
+                if(!message.guild.member(message.author.id).hasPermission("MANAGE_CHANNELS")) return message.channel.send("**ليس لديك صلاحيات . :x:**");
+                        message.channel.send(`**${args}. لقد تم تشغيل الروم هذا للترحيب.**`);
+                    client.on("guildMemberRemove", (member) => {
+                            if(member.user.bot) return;
+                         var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('Member Leave')
+  .setDescription('GoodBye')
+  .addField('**Member ID:',"" +  member.user.id, true)
+    .addField('**Member Tag:', member.user.discriminator, true)
+    .addField('Created At', member.user.createdAt, true)
+    .addField(' :bust_in_silhouette:  Your Number',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+  .setFooter(member.guild.name, member.guild.iconURL, true)
+                         
+   args.send({embed : embed});
+                    });
+    }
 });
