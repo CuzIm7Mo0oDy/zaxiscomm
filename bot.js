b628f5cc1a4a94f1bf1afad4cde952b95efdfe63
HactorMC <44316275+HactorMC@users.noreply.github.com>
2018-12-06 11:06:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,3 +33,37 @@ client.user.setGame(`Zaxis Community  `,"https://www.twitch.tv/dggamingbot")
 
 
 client.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+  if (message.guild) {
+ let embed = new Discord.RichEmbed()
+  let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+  if (!args[1]) {
+message.channel.send("**Usage: !bc [message]**");
+return;
+}
+      message.guild.members.forEach(m => {
+ if(!message.member.hasPermission('ADMINISTRATOR')) return;
+          var bc = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .addField(' Server', `${message.guild.name}`, true)
+          .addField(' Sender ', `${message.author.username}!${message.author.discriminator}`, true)
+          .addField(' Message ', args)
+          .setThumbnail(message.guild.iconURL)
+          .setColor('RANDOM')
+          m.send(`${m}`,{embed: bc});
+      });
+      const unknown = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setTitle('✅| the messege is loading ')
+      .addBlankField(true)
+      .addField('♨| i got sended to  ', message.guild.memberCount , true)
+      .addField('📝| the message ', args)
+      .setColor('RANDOM')
+      message.channel.sendEmbed(embed);
+  }
+  } else {
+      return;
+  }
+});
